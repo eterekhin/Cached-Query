2d32c7026d362b94c9dbf0c230a96a28aed99aca
Evgeny Terekhin <eter55@yandex.ru>
2019-04-24 08:09:10 +0300

    .

--- HabrCacheQueryInfrastructure/CacheQuery/CacheQuery.cs
+++ HabrCacheQueryInfrastructure/CacheQuery/CacheQuery.cs
@@ -28,8 +28,9 @@ namespace HabrCacheQuery.ServiceCollectionExtensions
 
     public class QueryCache<TIn, TOut> : BaseCacheQuery<TIn, TOut>
     {
-        public QueryCache(IConcurrentDictionaryFactory<TIn, TOut> factory, IQuery<TIn, TOut> query)
-            : base(factory, query)
+        public QueryCache(IQuery<TIn, TOut> query,
+            IConcurrentDictionaryFactory<TIn, TOut> factory)
+            : base(query, factory)
         {
         }
     }
@@ -37,8 +38,9 @@ namespace HabrCacheQuery.ServiceCollectionExtensions
     public class AsyncQueryCache<TIn, TOut> : BaseCacheQuery<TIn, Task<TOut>>, IAsyncQuery<TIn, TOut>
     {
         public AsyncQueryCache(
-            IConcurrentDictionaryFactory<TIn, Task<TOut>> factory,
-            IAsyncQuery<TIn, TOut> query) : base(factory, query)
+            IAsyncQuery<TIn, TOut> query,
+            IConcurrentDictionaryFactory<TIn, Task<TOut>> factory)
+            : base(query, factory)
         {
         }
     }
@@ -49,41 +51,5 @@ namespace HabrCacheQuery.ServiceCollectionExtensions
         public bool Equals(TKey x, TKey y) => DeepEqualsCommonType(x, y);
 
         public int GetHashCode(TKey obj) => Hash.GetHashCode(obj);
-    }
-
-    public class Key
-    {
-        protected bool Equals(Key other)
-        {
-            return Field1 == other.Field1 && Field2 == other.Field2;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((Key) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (Field1 * 397) ^ Field2;
-            }
-        }
-
-        public int Field1 { get; set; }
-        public int Field2 { get; set; }
-    }
-
-    public class Value
-    {
-    }
-
-    public class SimpleCache
-    {
-        private ConcurrentDictionary<Key, Value> _cache { get; set; }
     }
 }
